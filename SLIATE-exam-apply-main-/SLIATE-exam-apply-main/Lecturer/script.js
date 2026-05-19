e6c831b91c2139bfe3fb6d717c436f65f531ea94
Piyumi Hansani <piyumilokuge109@gmail.com>
2026-05-19 23:54:52 +0530

    Delete SLIATE-exam-apply-main-/SLIATE-exam-apply-main directory

--- SLIATE-exam-apply-main-/SLIATE-exam-apply-main/Lecturer/script.js
+++
@@ -1,6 +0,0 @@
-           // navigation bar profile menu dropdown
-           let submenu = document.getElementById("sub-menu-wrap");
-
-           function togglemenu() {
-           submenu.classList.toggle("open-class");
-           }
